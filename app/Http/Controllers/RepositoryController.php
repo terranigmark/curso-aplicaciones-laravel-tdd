1d085e355b9528f7080f277b138597ed17958bb8
italomoralesf <profesoritalomoralesf@gmail.com>
2020-11-24 15:57:03 -0500

    Eliminar un registro

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -30,4 +30,11 @@ class RepositoryController extends Controller
 
         return redirect()->route('repositories.edit', $repository);
     }
+
+    public function destroy(Repository $repository)
+    {
+        $repository->delete();
+
+        return redirect()->route('repositories.index');
+    }
 }
--- tests/Feature/Http/Controllers/RepositoryControllerTest.php
+++ tests/Feature/Http/Controllers/RepositoryControllerTest.php
@@ -83,4 +83,22 @@ class RepositoryControllerTest extends TestCase
             ->assertStatus(302)
             ->assertSessionHasErrors(['url', 'description']);
     }
+
+    public function test_destroy()
+    {
+        $repository = Repository::factory()->create();
+
+        $user = User::factory()->create();
+
+        $this
+            ->actingAs($user)
+            ->delete("repositories/$repository->id")
+            ->assertRedirect('repositories');
+
+        $this->assertDatabaseMissing('repositories', [
+            'id' => $repository->id,
+            'url' => $repository->url,
+            'description' => $repository->description,
+        ]);
+    }
 }
