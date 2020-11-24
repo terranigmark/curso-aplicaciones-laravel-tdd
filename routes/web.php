f0fb2256e060128d167ee1aba277ad2304274c1b
italomoralesf <profesoritalomoralesf@gmail.com>
2020-11-24 14:45:09 -0500

    Proteger Rutas

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+Route::resource('repositories', App\Http\Controllers\RepositoryController::class)
+    ->middleware('auth');
---
+++ tests/Feature/Http/Controllers/RepositoryControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RepositoryControllerTest extends TestCase
+{
+    public function test_guest()
+    {
+        $this->get('repositories')->assertRedirect('login');        // index
+        $this->get('repositories/1')->assertRedirect('login');      // show
+        $this->get('repositories/1/edit')->assertRedirect('login'); // edit
+        $this->put('repositories/1')->assertRedirect('login');      // update
+        $this->delete('repositories/1')->assertRedirect('login');   // destroy
+        $this->get('repositories/create')->assertRedirect('login'); // create
+        $this->post('repositories', [])->assertRedirect('login');   // guardar
+    }
+}
