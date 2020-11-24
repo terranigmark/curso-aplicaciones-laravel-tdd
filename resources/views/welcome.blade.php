d94e4783bcb3fd5c1ac13ef9c012c5e266c40387
italomoralesf <profesoritalomoralesf@gmail.com>
2020-11-24 17:38:58 -0500

    Diseño con Tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,22 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body class="bg-gray-200">
-        @foreach($repositories as $repository)
-        <h2>{{ $repository->url }}</h2>
-        <p>{{ $repository->description }}</p>
-        @endforeach
+        <ul class="max-w-lg bg-white border-r border-gray-300 shadow-xl">
+            @foreach($repositories as $repository)
+            <li class="flex items-center text-black p-2 hover:bg-gray-300">
+                <img 
+                    src="{{ $repository->user->profile_photo_url }}" 
+                    class="w-12 h-12 rounded-full mr-2"
+                >
+                <div class="flex justify-between w-full">
+                    <div class="flex-1">
+                        <h2 class="text-sm font-semibold texti-back">{{ $repository->url }}</h2>
+                        <p>{{ $repository->description }}</p>
+                    </div>
+                    <span class="text-xs font-medium text-gray-600">{{ $repository->created_at }}</span>
+                </div>
+            </li>
+            @endforeach
+        </ul>
     </body>
 </html>
