13c645de0c1a8979cd4bf5e5f281b45ae3a6ac8f
italomoralesf <profesoritalomoralesf@gmail.com>
2020-11-24 18:34:21 -0500

    Ajustes Finales

--- resources/views/repositories/index.blade.php
+++ resources/views/repositories/index.blade.php
@@ -7,12 +7,21 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <p class="text-right mb-4">
+                <a 
+                    href="{{ route('repositories.create') }}" 
+                    class="bg-blue-500 text-white font-bold py-2 px-4 rounded-md text-xs"
+                >
+                    Agregar un nuevo repositorio
+                </a>
+            </p>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
                 <table>
                     <thead>
                         <tr>
                             <th>ID</th>
                             <th>Enlace</th>
+                            <th>&nbsp;</th>
                             <th>&nbsp;</th>
                             <th>&nbsp;</th>
                         </tr>
@@ -32,10 +41,17 @@
                                     Editar
                                 </a>
                             </td>
+                            <td class="px-4 py-2">
+                                <form action="{{ route('repositories.destroy', $repository) }}" method="POST">
+                                    @csrf 
+                                    @method('DELETE')
+                                    <input type="submit" value="Eliminar" class="px-4 rounded-md bg-red-500 text-white">
+                                </form>
+                            </td>
                         </tr>
                         @empty
                         <tr>
-                            <td colspan="4">No hay repositorios creados</td>
+                            <td colspan="5">No hay repositorios creados</td>
                         </tr>
                         @endforelse
                     </tbody>
